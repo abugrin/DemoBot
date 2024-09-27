6222f21b86a395ec061f5663f7767d0f13db24c6
Anton <abugrin@yandex.ru>
2024-09-27 17:04:48 +0300

    Upd

--- src/main/java/com/yandex/bot/service/DemoBotService.java
+++ src/main/java/com/yandex/bot/service/DemoBotService.java
@@ -5,7 +5,6 @@ import com.yandex.bot.entity.Update;
 import com.yandex.bot.entity.Updates;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
